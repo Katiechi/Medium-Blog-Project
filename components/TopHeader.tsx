4466566e471abc058f87c035480e79c63b881e64
Brian <brian.katiechi9@gmail.com>
2023-06-12 06:51:33 +0300

    Changed the inline style csss for the logo to fit at the top left, changed the Home.module.css code

--- components/TopHeader.tsx
+++ components/TopHeader.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 function TopHeader() {
   return (
-    <div>TopHeader</div>
+    <header>
+      <div>
+        <Link href="/">
+          <img style={{ width: '150px', height: '50px' }}  src='https://ucbd382d867eda2285bc3724c2e9.previews.dropboxusercontent.com/p/thumb/AB65a58uuB__FZbQe9It2Fkwt7CznurhBFTguSYIpZFc6T4H02YBA2JoxDUM7xBR35bDzY3y30030133hdU5hKLBu5FpdrybMgrlrTF-dC-J08Or8UhNhPYbR9sGwARQEmkDYDNu0ofWSqdTOCrdgUp0OAIrWnlHiRmSAXM-tEMLkHNygBoWlcTKTUdPAESEC5jdXYsyK-q5Ymxa57WY4UJHzzcmzRA5yDEiKMOCo6BEJtf5dWHXzU68qsvlOk58hIbpWNRWmJsowwx2Luq7FtiSiTkE8DGjaV2lvIT_9il0bFhok7e-sB-9xq6CNP79v7BTqHahuA1xyzW9n8Fkb6dxTBlMHzxP0od95ZjmwOzmP8dMOPamlyEW0e7dvGUflSv9WUK2dncUekC8MWkITJMxTyOjYGOf3rzxPyUAx3pjuVNI12uCN-1eLA-UAi5NLETM7pyFX4CyrSmfaxmLFnm6TI9QZRsnA4sqp7iQ6rtojftL73dp4CNsAz6SgcHfkY7rJaTRFMv7DyMDQXNT6OneanVk_V3BZSzHCK1mBXhyGpRp8jhg3dvv5v-tKJoYW-Eb4pxWf9Cw9-gDSYnLLAUyr3lZsjM0VMejbsIdH1LpBx37DMj3brP-afm9G36pokKI_-oFpCaCeNck6_UxVkjbdzvZOiQ7j28V_X878QYsyypop50u8G9DofgLIyaa0-0mbea3sjbV5gLhRlAkZfRkLwmSRrNRao2MNWWU7fyoY_rwdkRms-DmT7ox-mrPmb8v0IKDmWhm3Hc8Q8Pcu60mLgAGf0TprN5ZkJePNEE1Id9ZMd8kY59RWrsj72BY7aNVsPBDAkU9YCTV_Aw5ao0f7wTbj7Ydc1mABTts0v1ELznnB-GEd4_HW-H3075dNAIlX0eXAAVc_eLiEJnB1iZp-McMmDs8oVrS-I1tcntRPDqDyKnK4UsSU-7pze_ID78eW60uzQGb_pnG9RarN0q1wEp73GZoBoec9LWV89qdmeZ_g0ujjOVGIGtM0WYh7RuIVLZtKwh13xiyySCc_4JyCUPMfDcuZskA1CTMTY1E2HNREP6FUpl9yC08B2M2E4J0QsLjiP5oOwDdyB0qaZElJtf7vPGg3irhqQXZ768YodhRKf1fuH7R57ZmE9S15uoaNNNzGlYyvsXn7kxrC4QYAoVAN0DFHtnB06TvZsMCJPkYkOHRs22UbgVyNFCSyDKSREFeb-L0hraEnaRv8qzUQCjSLiumsdEAzv_oK-RSSA/p.png' />
+       </Link>
+      </div>
+
+    </header>
+    
   )
 }
 
-export default TopHeader
-
+export default TopHeader;
